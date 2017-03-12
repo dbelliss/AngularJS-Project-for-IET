d732b1e924ca497c039ed2e926a59d7c253bf517
dbelliss <daveybelliss@yahoo.com>
2017-03-11 20:53:23 -0800

    Able to get data form weather api

--- src/app/hello.js
+++ src/app/hello.js
@@ -1,6 +1,11 @@
 export const hello = {
   template: require('./hello.html'),
-  controller() {
-    this.hello = 'Hello World!';
+  controller($scope, $http, $log) {
+    $scope.hello = 'Hello World!';
+    this.successCallback = function (response) {
+      $scope.hello = angular.fromJson(response);
+      $log.log(angular.fromJson(response));
+    };
+    $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?id=5341704&cnt=2&APPID=b35f2b4ea7c48895bd3d4e23d86e733e').then(this.successCallback, this.successCallback);
   }
 };
