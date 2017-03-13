c962f38efb44956c6f2e68ae7b9c13cdbccd4661
dbelliss <daveybelliss@yahoo.com>
2017-03-13 00:40:55 -0700

    Added links to credits

--- src/app/credits.js
+++ src/app/credits.js
@@ -4,15 +4,25 @@ export const credits = {
   template: require('./credits.html'),
   controller() {
     this.path = './app/images/';
-    this.images = [
+    this.fileNames = [
       'angular-logo.svg',
       'yeoman-logo.png',
       'fountain-logo.png',
       'gulp-logo.png',
-      'babel-logo.png'
+      'babel-logo.png',
+      'open-weather-map-logo.png'
     ];
-    for (let i = this.images.length - 1; i >= 0; i--) {
-      this.images[i] = this.path + this.images[i];
+    this.source = [
+      'https://angularjs.org/',
+      'http://yeoman.io/',
+      'https://github.com/FountainJS/generator-fountain-webapp',
+      'http://gulpjs.com/',
+      'https://babeljs.io/docs/plugins/preset-es2015/',
+      'https://openweathermap.org/api'
+    ];
+    this.images = [];
+    for (let i = this.fileNames.length - 1; i >= 0; i--) {
+      this.images[i] = this.path + this.fileNames[i];
     }
   }
 };
