cf5179b30899a5b903913c5d27c2d6cd5cefbc98
dbelliss <daveybelliss@yahoo.com>
2017-03-17 19:09:59 -0700

    Printing aggiefeed object to console

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {credits} from './app/credits';
 import './index.css';
 export const app = 'app';
 /*  eslint angular/di: "off"  */
-
+/* eslint no-unused-vars: "off" */
 angular
   .module(app, ['ngMaterial'])
   .service('weatherService', function ($http, $log) {
@@ -15,10 +15,78 @@ angular
         rootThis.weatherList = angular.fromJson(response).data;
         $log.log(angular.fromJson(response).data);
         for (let i = 0; i < rootThis.weatherList.list.length; i++) {
-          $log.log(i);
+          // $log.log(i);
           rootThis.weatherList.list[i].image = 'http://openweathermap.org/img/w/' + rootThis.weatherList.list[i].weather[0].icon + '.png';
-          $log.log(rootThis.weatherList.list[i].image);
+          // $log.log(rootThis.weatherList.list[i].image);
         }
+    // Creating aggiefeed data to send
+        const data = {
+          icon: 'https://upload.wikimedia.org/wikipedia/commons/1/15/OpenWeatherMap_logo.png',
+          actor: {
+            objectType: 'department',
+            displayName: 'all',
+            image: {
+              color: '#ffffff'
+            },
+            author: {
+              id: 1,
+              displayName: 'Open Weather'
+            }
+          },
+          verb: 'post',
+          title: 'Daily Forecast',
+          generator: {
+            id: '5'
+          },
+          object: {
+            ucdSrcld: 6,
+            objectType: 'notification',
+            content: 'dsadsdsafdsfa',
+            contentImage: {
+              dimensions: {
+                normal: {
+                  url: 'dsafdsf',
+                  width: 400,
+                  height: 300
+                }
+              },
+              source: 'Open Weather'
+            },
+            ucdEdusModel: {
+              url: 'http://api.openweathermap.org/data/2.5/forecast/daily?id=5341704&cnt=7&APPID=b35f2b4ea7c48895bd3d4e23d86e733e',
+              urlDisplayName: 'Daily forecast for Davis',
+              event: {
+                hasStartTime: false,
+                location: 'Davis',
+                startDate: 'dsajdkhsadhaskdjhsaldhska',
+                endDate: null,
+                isAllDay: true
+              }
+            },
+            location: {
+              displayName: 'Davis',
+              geo: {
+                latitude: '38.5382',
+                longitude: '121.7617'
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [121.7617, 38.5382]
+              }
+            }
+          },
+          to: {
+            id: 'public',
+            g: true,
+            i: false
+          },
+          ucdEdusMeta: {
+            labels: ['weather'],
+            startDate: 'dsadsfdsafsdafdasfasdf',
+            endDate: 'dsahfjkdhsflkjhasfk'
+          }
+        };
+        $log.log(data);
       };
       $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?id=5341704&cnt=7&APPID=b35f2b4ea7c48895bd3d4e23d86e733e').then(this.successCallback, this.successCallback);
     };
