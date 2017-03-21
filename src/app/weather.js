b7692341755012001e212999796c46be9fd6be6b
dbelliss <daveybelliss@yahoo.com>
2017-03-21 02:58:52 -0700

    Simple implementation of part 4 done

--- src/app/weather.js
+++ src/app/weather.js
@@ -2,9 +2,16 @@ import './weather.css';
 
 export const weather = {
   template: require('./weather.html'),
-  controller(weatherService) {
+  controller(weatherService, $log) {
     weatherService.getWeather(this);
     this.cityName = '';
     this.weatherList = [];
+    this.x = 0;
+    this.update = function () {
+      $log.log('hi');
+      const temp = angular.copy(this.weatherList.list[0]);
+      temp.temp.max = this.x++;
+      this.weatherList.list.unshift(temp);
+    };
   }
 };
