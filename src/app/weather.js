7b94df31e97481ed0c6e44ce0dd620580234143d
dbelliss <daveybelliss@yahoo.com>
2017-03-22 15:42:11 -0700

    adding bootstrap glyphcon

--- src/app/weather.js
+++ src/app/weather.js
@@ -2,18 +2,13 @@ import './weather.css';
 
 export const weather = {
   template: require('./weather.html'),
-  controller(weatherService, $log) {
+  controller(weatherService) {
     weatherService.getWeather(this);
     this.cityName = '';
     this.weatherList = [];
-    this.x = 0;
     this.activities = [];
     this.update = function () {
-      $log.log('hi');
-      $log.log(this.activities);
-      const temp = angular.copy(this.activities[0]);
-      // temp.temp.max = this.x++;
-      this.activities.unshift(temp);
+      weatherService.getWeather(this);
     };
   }
 };
